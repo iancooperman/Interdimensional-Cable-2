f3b6b0e504aaabea99ea11d6862e3ba2237417f5
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-02-01 10:34:19 -0800

    Wrote a non-functional draft of the Crawler

---
+++ crawler.py
@@ -0,0 +1,25 @@
+from praw import Reddit
+import re
+import json
+
+
+class Crawler:
+    def __init__(self, subreddit_name: str, client_id: str, client_secret: str, user_agent: str):
+        reddit = Reddit(client_id, client_secret, user_agent)
+        self._subreddit = reddit.subreddit(subreddit_name)
+
+
+    def fetch_urls(self) -> list:
+        video_urls = []
+        for submission in self._subreddit.hot(limit=100):
+            url = submission.url
+            if re.match("https:\/\/www\.youtube\.com\/watch", url):
+                video_urls.append(url)
+
+        return video_urls
+
+if __name__ == '__main__':
+    file = open("crawler.json", "r")
+    info = json.load(file)
+    file.close()
+    crawler = Crawler("InterdimensionalCable", info["id"], info["secret"], info["agent"])
