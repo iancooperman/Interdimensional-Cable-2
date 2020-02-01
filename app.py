468ade4fcd77c4f32d356f3b6b7df44739d09267
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-02-01 11:42:34 -0800

    All fetched urls printing to web browser window

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from crawler import Crawler
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<strong>Hello World!</strong>'
+def index():
+    reddit_info_file = open("reddit.json", "r")
+    reddit_info = json.load(reddit_info_file)
+    reddit_info_file.close()
+
+    sub_crawler = Crawler("InterdimensionalCable", reddit_info["id"], reddit_info["secret"], reddit_info["agent"])
+    video_urls = sub_crawler.fetch_urls()
+
+    return render_template("index.html", urls=video_urls)
 
 
 if __name__ == '__main__':
