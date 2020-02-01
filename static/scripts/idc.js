4effad0b84a0cb916350f372232a2488122205d4
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-02-01 12:50:24 -0800

    IDK

--- static/scripts/idc.js
+++ static/scripts/idc.js
@@ -1,6 +1,11 @@
 var urls;
+var embeded_youtube_player;
 
 function init(video_urls) {
     urls = video_urls;
-    console.log(urls)
+    embeded_youtube_player = document.getElementById("ytplayer");
+}
+
+embeded_youtube_player.onload = function() {
+    embeded_youtube_player.src = urls[0];
 }
