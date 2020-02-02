03efa5eefdc86f9718b47b8475287296fbeb8716
Ian Cooperman <i.perry.leonard.c@gmail.com>
2020-02-01 19:20:40 -0800

    Added button tags

--- static/scripts/idc.js
+++ static/scripts/idc.js
@@ -1,11 +1,25 @@
 var IDs;
 var embededYoutubePlayer;
+var nextButton;
+var prevButton;
+
+
 var videoNum;
 var totalVideos;
 
 function setVideo(videoNumber) {
     embededYoutubePlayer.src = IDs[videoNumber];
     videoNum = videoNumber;
+}
+
+function nextVideo() {
+    videoNum++;
+    setVideo(videoNum % totalVideos);
+}
+
+function previousVideo() {
+    videoNum--;
+    setVideo(videoNum % totalVideos);
 }
 
 function loadFirstVideo() {
@@ -17,4 +31,9 @@ function init(videoIDs) {
     totalVideos = IDs.length;
     embededYoutubePlayer = document.getElementById("ytplayer");
     embededYoutubePlayer.onload = loadFirstVideo();
+
+    prevButton = document.getElementById("prev-button");
+    nextButton = document.getElementById("next-button");
+    prevButton.onclick = previousVideo();
+    nextButton.onclick = nextVideo()
 }
